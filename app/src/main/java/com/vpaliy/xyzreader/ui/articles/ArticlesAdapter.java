c9031eac9c522a081fda399998bb45efe7b9b483
vpaliy <vpaliy97@gmail.com>
2017-06-29 00:52:19 -0400

    presentation layer : : update main screen

--- app/src/main/java/com/vpaliy/xyzreader/ui/articles/ArticlesAdapter.java
+++ app/src/main/java/com/vpaliy/xyzreader/ui/articles/ArticlesAdapter.java
@@ -1,14 +1,31 @@
 package com.vpaliy.xyzreader.ui.articles;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.target.ImageViewTarget;
 import com.vpaliy.xyzreader.R;
 import com.vpaliy.xyzreader.domain.Article;
 import java.util.ArrayList;
 import java.util.List;
+import static android.support.v7.graphics.Palette.Swatch;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ViewHolder> {
@@ -16,7 +33,7 @@ public class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ViewHo
     private List<Article> data;
     private LayoutInflater inflater;
 
-    public ArticlesAdapter(Context context){
+    ArticlesAdapter(Context context){
         this.data=new ArrayList<>();
         this.inflater=LayoutInflater.from(context);
     }
@@ -26,15 +43,63 @@ public class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ViewHo
         notifyDataSetChanged();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.article_image)
+        ImageView image;
+
+        @BindView(R.id.article_title)
+        TextView articleTitle;
+
+        @BindView(R.id.article_author)
+        TextView articleAuthor;
+
+        @BindView(R.id.background)
+        View background;
 
         ViewHolder(View root){
             super(root);
+            ButterKnife.bind(this,root);
         }
 
         void bindData(){
+            Article article=at(getAdapterPosition());
+            articleTitle.setText(article.getTitle());
+            articleAuthor.setText(article.getAuthor());
 
+            Glide.with(itemView.getContext())
+                    .load(article.getPosterUrl())
+                    .asBitmap()
+                    .centerCrop()
+                    .priority(Priority.HIGH)
+                    .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                    .into(new ImageViewTarget<Bitmap>(image) {
+                        @Override
+                        protected void setResource(Bitmap resource) {
+                            image.setImageBitmap(resource);
+                            new Palette.Builder(resource)
+                                    .generate(ViewHolder.this::applyPalette);
+                        }
+                    });
         }
+
+        private void applyPalette(Palette palette){
+            if (palette != null) {
+                Swatch result=palette.getDominantSwatch();
+                if(palette.getDarkVibrantSwatch()!=null){
+                    result=palette.getDarkVibrantSwatch();
+                }
+                else if(palette.getDarkMutedSwatch()!=null){
+                    result=palette.getDarkMutedSwatch();
+                }
+                background.setBackgroundColor(result.getRgb());
+            }
+        }
+
+    }
+
+    private Article at(int index){
+        return data.get(index);
     }
 
     @Override
--- app/src/main/java/com/vpaliy/xyzreader/ui/view/RatioImageView.java
+++ app/src/main/java/com/vpaliy/xyzreader/ui/view/RatioImageView.java
@@ -1,30 +1,39 @@
 package com.vpaliy.xyzreader.ui.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 
+import com.vpaliy.xyzreader.R;
+
 public class RatioImageView extends AppCompatImageView {
 
-    private float imageRatio=1.5f;
+    //16:9
+    private float imageRatio=.5625f;
 
     public RatioImageView(Context context){
-        super(context);
+        this(context,null,0);
     }
 
     public RatioImageView(Context context, AttributeSet attrs){
-        super(context,attrs);
+        this(context,attrs,0);
     }
 
     public RatioImageView(Context context, AttributeSet attrs, int defStyle){
         super(context,attrs,defStyle);
+        if(attrs!=null){
+            TypedArray array=getContext().obtainStyledAttributes(attrs, R.styleable.RatioImageView);
+            imageRatio=array.getFloat(R.styleable.RatioImageView_image_ration,imageRatio);
+            array.recycle();
+        }
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int measuredWidth=getMeasuredWidth();
-        setMeasuredDimension(measuredWidth, (int) (measuredWidth / imageRatio));
+        setMeasuredDimension(measuredWidth, Math.round(measuredWidth*imageRatio));
     }
 
     public void setImageRatio(float imageRatio) {
